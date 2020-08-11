ffe27edb018103f1b48b8bf3c6866cfe1882b91f
MarcusUniversee <clubbersmarcus@gmail.com>
2020-08-10 18:56:45 -0700

    Changed to php
    Also added a template file for the header to make code easier to change

--- Main Pages/clubnews.php
+++ Main Pages/clubnews.php
@@ -22,36 +22,7 @@
         <h4 class="display-4 m-0 	d-block d-md-none">Goal Throw Club News</h4>
         <p class="lead"> The premier SoCal Water Polo Club news website</p>
         <!---->
-        <!--NAV BAR-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <!--NAV HEADER-->
-            <a class="navbar-brand" href="/Main Pages/index.html"><img src="https://i.ibb.co/Z8h9pg8/Goal-Throw-News.png" height="80" width="80" class="img-fluid pull-xs-left" alt="..."></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-            <!---->
-            <!--NAVBAR LINKS-->
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <!--HOME-->
-                    <li class="nav-item">
-                        <a class="nav-link" href="/Main Pages/index.html ">Home</a>
-                    </li>
-                    <!---->
-                    <!--CLUB NEWS-->
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/Main Pages/clubnews.html">Club News <span class="sr-only">(current)</span></a>
-                    </li>
-                    <!---->
-                    <!--NATIONAL NEWS-->
-                    <li class="nav-item">
-                        <a class="nav-link" href="/Main Pages/nationalnews.html">National News </a>
-                    </li>
-                </ul>
-            </div>
-            <!---->
-        </nav>
-        <!---->
+        <?php include "template.php"; ?>
     </div>
     <!-- Carousel -->
     <div class="container">
--- Main Pages/nationalnews.php
+++ Main Pages/nationalnews.php
@@ -19,36 +19,7 @@
         <p class="lead d-none d-md-block"> The premier Water Polo news website</p>
         <p class="d-block d-md-none">The premier Water Polo news website</p>
         <!---->
-        <!--NAV BAR-->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <!--NAV HEADER-->
-            <a class="navbar-brand" href="/Main Pages/index.html"><img src="https://i.ibb.co/Z8h9pg8/Goal-Throw-News.png" height="80" width="80" class="img-fluid pull-xs-left" alt="..."></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <!---->
-            <!--NAVBAR LINKS-->
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <!--HOME-->
-                    <li class="nav-item">
-                        <a class="nav-link" href="/Main Pages/index.html">Home</a>
-                    </li>
-                    <!---->
-                    <!--CLUB NEWS-->
-                    <li class="nav-item">
-                        <a class="nav-link" href="/Main Pages/clubnews.html">Club News</a>
-                    </li>
-                    <!---->
-                    <!--NATIONAL NEWS-->
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/Main Pages/nationalnews.html">National News  <span class="sr-only">(current)</span> </a>
-                    </li>
-                </ul>
-            </div>
-            <!---->
-        </nav>
-        <!---->
+        <?php include "template.php"; ?>
     </div>
     <!-- The picture that will replace the carousel when the screen is too small-->
     <div class="container d-block d-lg-none ">
