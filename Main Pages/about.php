10c9d7fedb6c2521b98888f58507b484af317a77
UlyssesH23 <59684036+UlyssesH23@users.noreply.github.com>
2020-08-21 15:00:22 -0700

    Update about.php

--- Main Pages/about.php
+++ Main Pages/about.php
@@ -2,20 +2,20 @@
 <html lang="en">
   <head>
     <title>About Goal Throw News</title>
-    <!-- Required meta tags -->
+    <!-- Required meta tags -->hh
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
-  <body> 
+  <body>
        <?php include "template.php"; ?>
   <h1 class="display-1 m-0 d-none d-lg-block text-center border-bottom border-dark">About</h1>
         <h2 class="display-2 m-0 d-none d-md-block d-lg-none text-center ">About</h2>
         <h4 class="display-4 m-0 d-none d-sm-block d-md-none text-center ">About</h4>
         <h5 class="display-5 m-0  d-block d-sm-none text-center ">About</h5>
-      
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
