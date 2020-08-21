2d1cfa183c702abb65599d42af83aa4d0c92637d
UlyssesH23 <59684036+UlyssesH23@users.noreply.github.com>
2020-08-21 15:26:38 -0700

    About update

--- Main Pages/about.php
+++ Main Pages/about.php
@@ -9,13 +9,20 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
-  <body> 
+  <body>
        <?php include "template.php"; ?>
   <h1 class="display-1 m-0 d-none d-lg-block text-center border-bottom border-dark">About</h1>
         <h2 class="display-2 m-0 d-none d-md-block d-lg-none text-center ">About</h2>
         <h4 class="display-4 m-0 d-none d-sm-block d-md-none text-center ">About</h4>
         <h5 class="display-5 m-0  d-block d-sm-none text-center ">About</h5>
-      
+
+        <h3 class="display-2 m-0 d-none d-lg-block text-center border-bottom border-dark">What is Goal Throw News?</h2>
+              <h4 class="display-2 m-0 d-none d-md-block d-lg-none text-center ">What is Goal Throw News</h3>
+              <h5 class="display-4 m-0 d-none d-sm-block d-md-none text-center ">What is Goal Throw News?</h5>
+              <h6 class="display-5 m-0  d-block d-sm-none text-center ">What is Goal Throw News?</h6>
+              <p>Goal Throw News is a news website dedicated to Water Polo News.</p>
+
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
