dd9867e2c2ca4963fef4f28330aedadda1c56008
UlyssesH23 <59684036+UlyssesH23@users.noreply.github.com>
2020-08-30 17:04:44 -0700

    Update about.php

--- Main Pages/about.php
+++ Main Pages/about.php
@@ -10,17 +10,20 @@
   </head>
   <body>
        <?php include "template.php"; ?>
-       <br>
-       <br>
-  <h1 class="display-1 m-0 d-none d-lg-block text-center mb-4">About</h1>
-        <h2 class="display-2 m-0 d-none d-md-block d-lg-none text-center ">About</h2>
-        <h4 class="display-4 m-0 d-none d-sm-block d-md-none text-center ">About</h4>
-        <h5 class="display-5 m-0  d-block d-sm-none text-center ">About</h5>
 
-        <h4 class="display-4 m-0 d-none d-lg-block text-center mt-4 pt-4 text-decoration-underlined"><u class="sub-title mt-3">We are Goal Throw News</u></h4>
+  <div class="container">
+
+  <h1 class="display-1 m-0 d-none d-lg-block ">About</h1>
+        <h2 class="display-2 m-0 d-none d-md-block d-lg-none ">About</h2>
+        <h4 class="display-4 m-0 d-none d-sm-block d-md-none  ">About</h4>
+        <h5 class="display-5 m-0  d-block d-sm-none  ">About</h5>
+        <hr>
+</div>
+        <h4 class="display-4 m-0 d-none d-lg-block text-center mt-4 pt-4 text-decoration-underlined">We are Goal Throw News</h4>
               <h4 class=" m-0 d-none d-md-block d-lg-none text-center  mt-4">We are Goal Throw News</h3>
               <h5 class=" m-0 d-none d-sm-block d-md-none text-center  mt-4">We are Goal Throw News</h5>
               <h6 class="display-6 m-0  d-block d-sm-none text-center  mt-4">We are Goal Throw News</h6>
+
               <div class="container mx-5 px-5">
 
               <p class="text-center text-indent mt-4">Goal Throw News is a news website dedicated to covering all things Water Polo. We cover club, highschool, and national waterpolo events. We are based in Southern California and were founded
