9ff8bec5310eb8b8f9d80e379dc4db2d6bc04845
UlyssesH23 <59684036+UlyssesH23@users.noreply.github.com>
2020-09-16 12:45:25 -0700

    Changes to the Story example pages
    I fixed the welcome story  including moving the text and changing the size of the text.

--- Story Pages/welcomeStory.php
+++ Story Pages/welcomeStory.php
@@ -5,30 +5,32 @@
     <title>Goal Throw News</title>
     <!-- META TAGS -->
     <meta charset="utf-8">
-  <?php include "headerlinks.php"; ?>
+  <?php include "../Main Pages/headerlinks.php"; ?>
 </head>
 
 <body>
-    <div class="container py-5">
+    <?php include "../Main Pages/template.php"; ?>
+    <div class="container py-2">
+
         <!--HEADING-->
-        <h1 class="display-1 m-0 d-none d-lg-block">Goal Throw News</h1>
+    <!--<h1 class="display-1 m-0 d-none d-lg-block">Goal Throw News</h1>
         <h2 class="display-2 m-0 d-none d-md-block d-lg-none">Goal Throw News</h2>
         <h4 class="display-4 m-0 d-block d-md-none">Goal Throw News</h4>
         <p class="lead"> The premier Water Polo news website</p>
-        </div>
+      </div>      -->
         <!---->
         <!--NAV BAR-->
-        <?php include "template.php"; ?>
-<div class="container">
-        <img class="w-100" src="https://i.ibb.co/nMmfPVK/Goal-Throw-News-Copy.jpg" alt="">
-        </div>
 
     <div class="container">
 
-      <h3 class="display-3 m-0 d-none d-lg-block text-center">Welcome</h3>
-      <h4 class="display-4 m-0 d-none d-md-block d-lg-none text-center">Welcome</h4>
+      <h3 class="display-3 m-0 mb-3 d-none d-lg-block text-center ">Welcome</h3>
+      <h4 class="display-4 m-0 mb-3 d-none d-md-block d-lg-none text-center">Welcome</h4>
       <h5 class="display-5 m-0 	d-block d-md-none">Welcome</h5>
 </div>
+      <div class="container text-center w-80">
+              <img class="img-responsive " src="../Goal Throw News (1).png" alt="">
+              </div>
+
     <div class="article-body">
       <p class="article-paragraph">
         Welcome to goal throw news!
