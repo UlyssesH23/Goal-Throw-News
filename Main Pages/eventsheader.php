e30927efdf1406e315824113e8ebf1df3a262b15
UlyssesH23 <59684036+UlyssesH23@users.noreply.github.com>
2020-09-17 12:23:45 -0700

    Test

--- Main Pages/eventsheader.php
+++ Main Pages/eventsheader.php
@@ -1,3 +1,3 @@
 <h3 class="display-3 m-0 text-center d-none d-lg-block" id="latest-news">Latest News</h3>
-<h4 class="display-4 m-0 d-none text-center d-md-block d-lg-none" id="latest-news">Latest News</h4>
+<h4 clacccss="display-4 m-0 d-none text-center d-md-block d-lg-none" id="latest-news">Latest News</h4>
 <h5 class="display-5 m-0 	d-block text-center d-md-none" id="latest-news">Latest News</h5>
