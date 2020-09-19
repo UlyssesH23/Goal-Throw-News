02b88615052109bae0e3094d837132bd56711741
UlyssesH23 <59684036+UlyssesH23@users.noreply.github.com>
2020-09-18 20:20:28 -0700

    Changes to carousel and some js tests

--- Stylesheets/style.js
+++ Stylesheets/style.js
@@ -1,1 +1,15 @@
-document.querySelector("h1").style.color ="red";
+
+var searchBtn = document.getElementsByTagName('button')[2];
+searchBtn.addEventListener('click',function (event){
+  var chewbacaSound = new Audio ('../Chewbacca sound-1.mp3');
+  chewbacaSound.play()
+  console.log("chwebaca sound")
+})
+
+// function counts(){
+//   for (var i = 1; i < 10 ; i++){
+// console.log(i);
+//   }
+// }
+// counts();
+// counts();
